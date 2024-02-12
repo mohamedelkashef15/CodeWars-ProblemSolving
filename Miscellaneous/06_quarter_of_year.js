4a90d06038d4e71d0b408c4f97b5db18c2140d20
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-02-12 23:55:59 +0200

    Add problem 8

---
+++ Miscellaneous/06_quarter_of_year.js
@@ -0,0 +1,29 @@
+/*
+  Problem definition
+  => Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+  - Algorithms
+  - check if value of month from 1 to 12 | if not then stop the program and print enter correct input 
+
+  - check the value of month if it's from 1 to 4(not included) then print quarter 1
+  - else if value of month from 4 - 7 then print quarter 2
+  - else if value of month from 7 - 10 then print quarter 3
+  - else if value of month from 10 - 12 then print quarter 4
+  - else print correct month
+*/
+
+const quarterOf = (month) => {
+  if (month >= 1 && month <= 12) {
+    if (month >= 1 && month < 4) {
+      return 1;
+    } else if (month >= 4 && month < 7) {
+      return 2;
+    } else if (month >= 7 && month < 10) {
+      return 3;
+    } else if (month >= 10 && month <= 12) {
+      return 4;
+    }
+  } else {
+    return "Enter correct month";
+  }
+};
