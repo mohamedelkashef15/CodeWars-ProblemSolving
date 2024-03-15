009b4fb859bb5a62c5dcef223d28e87bd06f9a89
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-03-15 23:57:17 +0200

    Add problem 21

---
+++ strings/10_is_string_uppercase.js
@@ -0,0 +1,16 @@
+/*
+  Problem definition
+  => Create a method to see whether the string is ALL CAPS.
+
+  Algorithms 
+  - loop through each char 
+  - check if one char is lower case then return false else return true 
+*/
+
+String.prototype.isUpperCase = function () {
+  return this.valueOf() === this.toUpperCase();
+};
+
+function isUpperCase(str) {
+  return str === str.toUpperCase();
+}
