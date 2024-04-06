f6a03110273e12d7122856907f7940f9ff03a5f5
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-04-06 12:03:54 +0200

    add problem 39

--- arrays/08_sum_of_differences.ts
+++ arrays/08_sum_of_differences.ts
@@ -3,18 +3,18 @@
   => Your task is to sum the differences between consecutive pairs in the array in descending order.
 
   Algorithms
-  - rearrange items from decending order
-  - arr[]
+  - rearrange items from decending order using sort
+  - loop through array and then return arr[i] - arr[i + 1]
 */
 
 export function sumOfDifferences(arr: number[]): number {
-  let sortedArr = arr.sort((a, b) => b - a);
+  arr.sort((a, b) => b - a);
   let result = 0;
   if (arr.length <= 1) {
     return (result = 0);
   } else {
-    for (let i = 0; i < sortedArr.length - 1; i++) {
-      result += sortedArr[i] - sortedArr[i + 1];
+    for (let i = 0; i < arr.length - 1; i++) {
+      result += arr[i] - arr[i + 1];
     }
   }
   return result;
