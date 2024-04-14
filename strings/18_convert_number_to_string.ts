283a9c0634f01df8bec47506618f1b12c826e054
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-04-14 13:58:18 +0200

    Add Problem 51

---
+++ strings/18_convert_number_to_string.ts
@@ -0,0 +1,10 @@
+/*
+  Problem definition
+  => We need a function that can transform a number (integer) into a string.
+  What ways of achieving this do you know?
+
+*/
+
+export function numberToString(num: number): string {
+  return `${num}`;
+}
