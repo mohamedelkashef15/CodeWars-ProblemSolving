54620b1d26995921e2adb4e29f8f3e24f7aa4adf
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-06-28 18:42:58 +0300

    add problem 70

---
+++ arrays/18_convert_num_to_reversed_array.ts
@@ -0,0 +1,24 @@
+/*
+  Problem definition
+  => Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+  Alogrithms
+  - create new empty array 
+  - store numbers inside array
+  - split array
+  - return reversed numbers
+*/
+
+export const digitize = (n: number): number[] => {
+  let newStr = n.toString();
+  let newArr = [];
+  for (let i = 0; i < newStr.length; i++) {
+    newArr.push(+newStr[i]);
+  }
+
+  return newArr.reverse();
+};
+
+// Using map
+export const digitize2 = (n: number): number[] => {};
+console.log(digitize(35231));
