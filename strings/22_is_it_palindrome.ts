b94651df5c0133505406d0fc67a7e4a40c8afe79
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-06-29 20:01:56 +0300

    add problem 71

---
+++ strings/22_is_it_palindrome.ts
@@ -0,0 +1,32 @@
+/*
+  Problem definition
+  => Write a function that checks if a given string (case insensitive) is a palindrome.
+
+    A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+
+  - Alogrithms
+    - loop on string from start to end
+    - loop on string from end to start
+    - check if char[index] from start equals char[index] from end
+    - if so then return true otherwise return false
+*/
+export function isPalindrome(x: string): boolean {
+  let str = x.toLowerCase();
+  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
+    if (str[i] !== str[j]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Another way
+export function isPalindrome2(x: string): boolean {
+  let str = x.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
